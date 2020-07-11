50769703244e29f4df0f52763a414681fbd17ada
Anul Jain <anuljain23@gmail.com>
2020-07-12 03:53:30 +0530

    added the BMI calculator app without functionality

---
+++ bmi-calculator-flutter/lib/components/bottombutton.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({@required this.onTap, @required this.buttonTitle});
+
+  final String buttonTitle;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: kBottomTextStyle,
+          ),
+        ),
+        color: kBottomContainerColour,
+        margin: EdgeInsets.only(top: 15.0),
+        padding: EdgeInsets.only(bottom: 20.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
---
+++ bmi-calculator-flutter/lib/constants.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 60.0;
+const kActiveCardColour = Color(0xFF1D1E33);
+const kInactiveCardColour = Color(0xFF111328);
+const kBottomContainerColour = Color(0xFFEB1555);
+
+const kLabelTextStyle = TextStyle(
+  color: Color(0xFF8D8E99),
+  fontSize: 18.0,
+);
+const kIconSize = 80.0;
+const kSpaceBetweenColumn = 15.0;
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kSliderMin = 120.0;
+const kSliderMax = 220.0;
+
+const kBottomTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 25.0,
+);
+
+const kTitleTextStyle =TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 40.0,
+);
+
+const kResultTextStyle = TextStyle(
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+  color: Color(0xFF24D786),
+);
+
+const kResultStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 70.0,
+);
+
+const kBodyTextStyle =TextStyle(
+  fontSize: 22.0,
+);
